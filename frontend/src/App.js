9d8614cefe0d84193e66512880a9e7a7c2bbfc61
a01752030 <a01752030@itesm.mx>
2023-10-30 03:51:07 -0600

    Fixed login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,75 +1,90 @@
-import React, { useState, useEffect } from 'react';
-import { CognitoUserPool, CognitoUser } from 'amazon-cognito-identity-js';
-import './App.css';
-import Login from './components/Login';
-import Dashboard from './components/Dashboard';
-import UserDashboard from './components/UserDashboard'
+import React, { useState } from 'react';
+import { useNavigate  } from 'react-router-dom';
+import { AuthenticationDetails, CognitoUser } from 'amazon-cognito-identity-js';
+import { CognitoUserPool } from 'amazon-cognito-identity-js';
 
 const poolData = {
-  UserPoolId: 'us-east-2_I8C2RUTQi', 
-  ClientId: '1vlrj7167oenhjh6a3sgor57ml'
+    UserPoolId: 'us-east-2_I8C2RUTQi',
+    ClientId: '1vlrj7167oenhjh6a3sgor57ml'
 };
+  
 const userPool = new CognitoUserPool(poolData);
 
-function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [userGroup, setUserGroup] = useState("");
+function Login() {
+    const [username, setUsername] = useState('');
+    const [PrefUser, setPrefUser] = useState('');
+    const [password, setPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [isChangingPassword, setIsChangingPassword] = useState(false);
+    const [cognitoUserState, setCognitoUserState] = useState(null);
+    const [Email, setEmail] = useState('');  
 
-    useEffect(() => {
-        const user = userPool.getCurrentUser();
-        if (user) {
-            user.getSession((err, session) => {
-                if (err) {
-                    console.error(err);
-                    return;
-                }
-                if (session.isValid()) {
-                    // This is a placeholder. In a real app, you would make a backend call here
-                    // to fetch the group(s) the user belongs to.
-                    fetchUserGroup(user.username)
-                        .then(group => {
-                            setIsLoggedIn(true);
-                            setUserGroup(group);
-                        })
-                        .catch(err => {
-                            console.error(err);
-                        });
-                }
-            });
-        }
-    }, []);
+    const history = useNavigate();
 
-    function fetchUserGroup(username) {
-        // This is a placeholder logic. In reality, you'd call your backend which in turn 
-        // calls AdminListGroupsForUser API of Cognito.
-        return new Promise((resolve, reject) => {
-            // Placeholder logic to mock group fetching:
-            setTimeout(() => {
-                if (username.startsWith('Admin')) {
-                    resolve('AdminGroup');
-                } else {
-                    resolve('UserGroup');
-                }
-            }, 1000);
+    const handleLogin = () => {
+        const authenticationDetails = new AuthenticationDetails({
+            Username: username,
+            Password: password
         });
-    }
 
-    function renderDashboard() {
-        switch(userGroup) {
-            case 'AdminGroup':
-                return <Dashboard />;
-            case 'UserGroup':
-                return <UserDashboard />;
-            default:
-                return null; // or some generic dashboard or loading state
+        const cognitoUser = new CognitoUser({
+            Username: username,
+            Pool: userPool
+        });
+
+        cognitoUser.authenticateUser(authenticationDetails, {
+            onSuccess: function () {
+                console.log('Login successful!');
+                history.push('/dashboard');
+            },
+            onFailure: function (err) {
+                console.error('Login failed:', err);
+            },
+            newPasswordRequired: function() {
+                console.log('Password change required');
+                setIsChangingPassword(true);
+                setCognitoUserState(cognitoUser);
+            }
+        });
+    };
+
+    const handlePasswordChange = () => {
+        if (!cognitoUserState) {
+            console.error("User session not available!");
+            return;
         }
-    }
+
+        const requiredAttributes = {
+            'nickname': 'default_nickname', // Replace this default value with appropriate logic for your use case
+        };
+
+        cognitoUserState.completeNewPasswordChallenge(newPassword, requiredAttributes, {
+            onSuccess: function() {
+                console.log('Password changed successfully');
+                setIsChangingPassword(false);
+                history.push('/dashboard');
+            },
+            onFailure: function(err) {
+                console.error('Password change failed:', err);
+            }
+        });
+    };
 
     return (
-        <div className="App">
-            { isLoggedIn ? renderDashboard() : <Login /> }
+        <div>
+            <input value={username} onChange={e => setUsername(e.target.value)} placeholder="Username" />
+            <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" />
+            {isChangingPassword && (
+                <>
+                    <input value={PrefUser} onChange={e => setPrefUser(e.target.value)} placeholder="Pref UserName" />
+                    <input value={Email} onChange={e => setEmail(e.target.value)} placeholder="Email" />
+                    <input type="password" value={newPassword} onChange={e => setNewPassword(e.target.value)} placeholder="New Password" />
+                    <button onClick={handlePasswordChange}>Change Password</button>
+                </>
+            )}
+            <button onClick={handleLogin}>Login</button>
         </div>
     );
 }
 
-export default App;
+export default Login;
